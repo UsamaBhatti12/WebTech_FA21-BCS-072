6f9df13e22b6de65cd51c2e2d7e1bc0130563442
UsamaBhatti12 <ub3994229@gmail.com>
2024-03-26 21:27:46 +0500

    done2

---
+++ labtask 1/Ajax Restful API Example.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+  // Making AJAX request to JSONPlaceholder API
+  $.get("https://jsonplaceholder.typicode.com/posts", function(data) {
+   
+    var output = '';
+    // creating cards dynamically and
+    // Loop to display returned data in these cards
+    $.each(data, function(index, post) {
+      output += '<div class="card mt-3">';
+      output += '<div class="card-body">';
+      output += '<h5 class="card-title">' + post.title + '</h5>';
+      output += '<p class="card-text">' + post.body + '</p>';
+      output += '</div>';
+      output += '</div>';
+    });
+    
+    $('#posts').html(output);
+  });
+});
+
